4c143dbf15f98526a8b3b36065002ac411f67502
tang3w <tang3w@gmail.com>
2013-04-19 02:17:59 +0800

    Add source files

---
+++ BlockSugar/BlockSugar.h
@@ -0,0 +1,13 @@
+//
+//  BlockSugar.h
+//  BlockSugar
+//
+//  Created by Tang Tianyong on 4/19/13.
+//  Copyright (c) 2013 Tang Tianyong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlockSugar : NSObject
+
+@end
